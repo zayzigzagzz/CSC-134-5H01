17c6496e8422d2ba4e12ff9c3804393f7c51c021
Isaiah McNeill <mcneilli4402@student.faytechcc.edu>
2025-01-23 14:21:07 +0000

    Pending changes exported from your codespace

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+//CSC-134
+//M1T2
+//Isaiah McNeill
+//1-23-25(since it didn't save and I never submitted)
+
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "My name is Isaiah McNeill" << endl;
+    cout << "I am enrolled in the Computer Science Transfer pathway" << endl;
+    cout << "At my school, Pine Forest High, I am a part of the National Honor Society!" << endl;
+
+    return 0;
+}
