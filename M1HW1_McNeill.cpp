26ce3aa25c4386249a12664e980d2e4c66f91bd8
Isaiah McNeill <mcneilli4402@student.faytechcc.edu>
2025-01-29 18:43:41 +0000

    finished movie details

---
+++ M1HW1_McNeill.cpp
@@ -0,0 +1,32 @@
+// CSC 134
+// M1HW1
+// Isaiah McNeill
+// 1-29-25
+// This program gives back movie statistics
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    string movie_title = "Sonic The Hedgehog: 3";
+    int release_year = 2024;
+    double earnings = 446.7;
+
+    cout << "I chose the movie " << movie_title << "." << endl;
+    cout << "It released in the year of " << release_year << "." << endl;
+    cout << movie_title << " earned a total of $" << earnings << " million." << endl;
+    cout << "Robotnik Reunion Scene:" << endl;
+    cout << "Gerald Robotnik: Welcome home, my boy." << endl;
+    cout << "Ivo Robotnik: It's impossible." << endl;
+    cout << "Gerald Robotnik: Is it?" << endl;
+    cout << "Ivo Robotnik: It couldn't be." << endl;
+    cout << "Gerald Robotnik: Couldn't it?" << endl;
+    cout << "Ivo Robotnik: I'm..." << endl;
+    cout << "Gerald Robotnik: Are you?" << endl;
+    cout << "Ivo Robotnik: [whispering] Pop-Pop?" << endl;
+    cout << "Gerald Robotnik: In the saggy flesh." << endl;
+
+    return 0;
+}
