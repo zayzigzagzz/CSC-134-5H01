a2239da028e8bf893aff34154703e803adaa30f0
Isaiah McNeill <mcneilli4402@student.faytechcc.edu>
2025-02-03 14:28:45 -0500

    Add files via upload

---
+++ demo_mcneill.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+t = Turtle()
+t.pensize(3)
+t.pencolor("blue")
+t.fillcolor("magenta")
+
+t.begin_fill() #fills the shape
+t.forward(300)
+t.left(90)
+t.forward(300)
+t.left(90)
+t.forward(300)
+t.left(90)
+t.forward(300)
+t.end_fill()
