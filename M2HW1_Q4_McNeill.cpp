c48a6d23dd5002d272a9dc0ca954102c64c4ece7
Isaiah McNeill <mcneilli4402@student.faytechcc.edu>
2025-02-19 16:43:58 +0000

    final product of Q4

---
+++ M2HW1_Q4_McNeill.cpp
@@ -0,0 +1,28 @@
+/*
+CSC-134
+M2HW1 - Silver
+Isaiah McNeill
+2/19/25
+*/
+
+#include <iostream>
+using namespace std;
+
+//Question 4
+int main()
+{
+    //variables
+    string letsGo = "Let's go";
+    string school = "FTCC";
+    string team = "Trojans";
+    string cheerOne;
+    string cheerTwo;
+
+    //calculations
+    cheerOne = letsGo + " " + school;
+    cheerTwo = letsGo + " " + team;
+
+    //output
+    cout << cheerOne << endl << cheerOne << endl << cheerOne << endl;
+    cout << cheerTwo << endl;
+}
