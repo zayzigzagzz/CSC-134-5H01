0e8908d6ba6c696dd88e87d25be057c79d1a83e1
Isaiah McNeill <mcneilli4402@student.faytechcc.edu>
2025-02-19 17:09:45 +0000

    final ver of m2hw1 question 3

---
+++ M2HW1_Q3_McNeill.cpp
@@ -0,0 +1,34 @@
+/*
+CSC-134
+M2HW1 - Silver
+Isaiah McNeill
+2/19/25
+*/
+
+#include <iostream>
+using namespace std;
+
+//Question 3
+int main()
+{
+    //variables
+    int pizzaNum, slices, partyPeople;
+
+    //input
+    cout << "How many pizzas do you want?" << endl;
+    cin >> pizzaNum;
+
+    cout << "How many slices per pizza?" << endl;
+    cin >> slices;
+
+    cout << "How many visitors will you have including yourself?" << endl;
+    cin >> partyPeople;
+
+    //calculations
+    int totalPizza = pizzaNum * slices;
+    int leftovers = totalPizza - (partyPeople * 3);
+
+    //output
+    cout << "You've ordered " << totalPizza << " slices. If everyone eats 3 slices of pizza, you will have " << leftovers << " slice/s left!" << endl;
+
+}
